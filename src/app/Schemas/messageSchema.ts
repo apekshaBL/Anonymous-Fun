bb52b63c64adf09eb38d0d72586806c72dc5e798
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2025-01-14 12:37:03 +0530

    created Schemas using zod validation

--- src/app/Schemas/messageSchema.ts
+++ src/app/Schemas/messageSchema.ts
@@ -1,1 +1,7 @@
 import { z } from "zod";
+
+export const messageSchema=z.object({
+    content:z.string()
+             .min(10,{message:"message should be of minimum 10 characters"})
+             .max(350,{message:"message should not be of more than 350 characters"})
+})
