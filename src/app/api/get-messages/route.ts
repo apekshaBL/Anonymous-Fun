c7d6b26304c4dc5813751786216a9b6610143038
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2025-01-23 07:08:41 +0530

    created aggregation pipeline

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,45 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import {User} from "next-auth";
+import { NextRequest } from "next/server";
+import mongoose from "mongoose";
+
+
+export async function GET(request:NextRequest){
+    await dbConnect();
+    const session=await getServerSession(authOptions);
+    const user:User=session?.user
+    if(!session || !session.user){
+        return Response.json({
+            success:false,
+            message:"Not authenticated"
+        },{status:401})
+    }
+
+    const userId=new mongoose.Types.ObjectId(user._id);
+    try{
+        const user=await UserModel.aggregate([
+            {$match:{id:userId}},
+            {$unwind:'$messages'},
+            {$sort:{'messages.createdAt':-1}},
+            {$group:{_id:'$_id',messages:{$push:'$messages'}}}
+        ])
+        if(!user || user.length===0){
+            return Response.json({
+                 success:false,
+                 message:"user not found"
+            },{status:401})
+        }
+
+        return Response.json({
+            success:true,
+            messages:user[0].messages
+        },{status:200})
+
+    }
+    catch(error){
+
+    }
+}
